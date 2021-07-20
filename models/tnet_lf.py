f87bddbf00c834bc1ce7f55ed05abb71f9b4e5c0
hlamba <hlamba@dataminr.com>
2021-07-19 23:18:54 -0400

    adding print

--- models/tnet_lf.py
+++ models/tnet_lf.py
@@ -73,7 +73,10 @@ class TNet_LF(nn.Module):
             aspect_mid = torch.cat((aspect_mid, v), dim=1).transpose(1, 2)
             print(type(aspect_mid))
             aspect_mid = aspect_mid.double()
+            aspect_mid = torch.tensor(aspect_mid).type('torch.FloatTensor')
+            print(aspect_mid)
             print(type(aspect_mid))
+
             aspect_mid = F.relu(self.fc1(aspect_mid).transpose(1, 2))
             print(type(aspect_mid))
             v = aspect_mid + v
